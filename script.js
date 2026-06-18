f89fa578bbb8bdf48c38946307af3a108888006b
manuelamaria-oss <manuela.maria@escola.pr.gov.br>
2026-06-18 11:05:35 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+const botoes =
+document.querySelectorAll(".like");
 
+botoes.forEach(botao=>{
+
+let curtidas=0;
+
+botao.addEventListener(
+"click",
+()=>{
+
+curtidas++;
+
+botao.nextElementSibling
+.textContent=
+`${curtidas} curtidas`;
+
+botao.innerHTML="💖 Curtido";
+
+});
+
+});
+
+
+const pesquisa=
+document.getElementById(
+"pesquisa"
+);
+
+pesquisa.addEventListener(
+"input",
+()=>{
+
+const valor=
+pesquisa.value
+.toLowerCase();
+
+document
+.querySelectorAll(".card")
+
+.forEach(card=>{
+
+const nome=
+card.querySelector("h2")
+.textContent
+.toLowerCase();
+
+card.style.display=
+nome.includes(valor)
+? "block"
+: "none";
+
+});
+
+});
